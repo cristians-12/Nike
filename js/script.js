f73c4cbc225f282e824aae80e06fc1a4761d47b5
Cristian <97061228+Eggward12@users.noreply.github.com>
2023-03-30 10:10:03 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const colores = document.querySelectorAll(".colores .color");
+const imagenes = ["img/Red Shoe.png","img/Yellow Shoe.png","img/morado.png"];
+const imagen = document.getElementById("Producto");
+
+colores[0].classList.add("activo");
+
+colores.forEach((cadacolor,i )=> {
+    colores[i].addEventListener('click',()=>{
+        colores.forEach((element,i)=>{
+            colores[i].classList.remove("activo");
+        })
+        colores[i].classList.add("activo");
+        imagen.setAttribute('src',imagenes[i]);
+    });
+});
